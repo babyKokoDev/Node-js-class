3ae6a39ee4947331b9a1fd923dec62679f78ca9a
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-30 14:06:23 +0100

    additional class

--- Express Js/node-express-course/02-express-tutorial/app.js
+++ Express Js/node-express-course/02-express-tutorial/app.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 
-
-app.use(express.static('./public'))
-// app.get('/', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// })
-
-app.all('*', (req, res)=> {
-    res.status(404).send('Request not found')
+app.get('/', (req, res)=> {
+    res.status(200).json([{name:'Joseph'}, {name:'Motunde'}])
 })
 
 
