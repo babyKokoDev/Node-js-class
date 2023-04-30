0434aeac3633c060a26a0a6a150f0528c681c868
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-30 13:51:39 +0100

    additional class

--- Express Js/node-express-course/02-express-tutorial/app.js
+++ Express Js/node-express-course/02-express-tutorial/app.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 
-app.get('/', (req, res)=> {
-    res.send('Home Page')
-})
-
-app.get('/about', (req, res)=> {
-    res.send('About Page')
-})
+app.use(express.static('./public'))
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
 
 app.all('*', (req, res)=> {
-     res.status(404).send('Page not found')
+    res.status(404).send('Request not found')
 })
 
+
 app.listen(5000, ()=> {
-    console.log('server is listening on port 5000')
+    console.log('Server is listening on port 5000')
 })
