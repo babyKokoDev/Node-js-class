cf30e38434fa8c2aa379303b45c303fd2a4df679
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-30 11:32:56 +0100

    additional class

--- Express Js/node-express-course/02-express-tutorial/app.js
+++ Express Js/node-express-course/02-express-tutorial/app.js
@@ -1,9 +1,11 @@
 const http = require('http');
+const { readFileSync } =require('fs')
+const homePage = readFileSync('./index.html')
 const server = http.createServer((req, res)=>{ 
     const url = req.url
     if (url === '/') {
         res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write('<h1>Home Page</h1>')
+        res.write(homePage)
         res.end()
     }
     else if (url === '/about') {
