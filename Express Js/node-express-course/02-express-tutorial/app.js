96c040626a66b2390b316b3eb79e63220d754a5c
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-06 19:38:43 +0100

    additional class

--- Express Js/node-express-course/02-express-tutorial/app.js
+++ Express Js/node-express-course/02-express-tutorial/app.js
@@ -4,8 +4,11 @@ const {products} = require('./data')
 
 
 app.get('/', (req, res)=>{
-    const newProducts = products.json()
-   res.status(200).json(newProducts)
+   res.send(`<h1>Home Page</h1> <a href="/api/products">products</a>`)
+})
+
+app.get('/api/products', (req, res)=>{
+    res.json(products)
 })
 
 
