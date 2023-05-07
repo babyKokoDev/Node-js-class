f8f41abf1446db8203147cadf9ed63d74d039089
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-07 12:28:39 +0100

    additional class

--- Express Js/node-express-course/02-express-tutorial/app.js
+++ Express Js/node-express-course/02-express-tutorial/app.js
@@ -3,25 +3,20 @@ const app = express();
 const {products} = require('./data')
 
 
-app.get('/', (req, res)=>{
-   res.send(`<h1>Home Page</h1> <a href="/api/products">products</a>`)
-})
-
-app.get('/api/products/:productID', (req, res)=>{
-    const {productID} = req.params
-    const singleProduct = products.find((product)=>{
-       return product.id === Number(productID)
-    })
-    if (singleProduct){
-        res.status(200).json(singleProduct)
-    }else {
-        res.status(404).send(`<h1>Product does not exist</h1>`)
-    }
-})
-
 app.get('/api/v1/query', (req, res)=>{
-    console.log(req.query)
-    res.send(`<h1>Home Page</h1>`)
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if (search){
+        sortedProducts = sortedProducts.filter((product)=>{
+           return product.name.startsWith(search)
+        })
+    }
+    if (limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+
+    res.status(200).json(sortedProducts)
 })
 
 
