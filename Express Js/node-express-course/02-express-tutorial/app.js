d36fd7fe491622f56820af57c1f2a9affff58ccf
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-07 19:43:30 +0100

    additional class

--- Express Js/node-express-course/02-express-tutorial/app.js
+++ Express Js/node-express-course/02-express-tutorial/app.js
@@ -1,29 +1,9 @@
 const express = require('express');
 const app = express();
-const {products} = require('./data')
 
-
-app.get('/api/v1/query', (req, res)=>{
-    const {search, limit} = req.query
-    let sortedProducts = [...products]
-
-    if (search){
-        sortedProducts = sortedProducts.filter((product)=>{
-           return product.name.startsWith(search)
-        })
-    }
-    if (limit){
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-
-    if (sortedProducts.length < 1){
-        res.status(200).json([{success : true, data : []}])
-    }
-
-    res.status(200).json(sortedProducts)
+app.get('/', (req, res) => {
+    
 })
-
-
 
 
 app.listen(5000, ()=> {
