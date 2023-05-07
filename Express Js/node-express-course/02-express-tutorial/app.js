7c22ab082e7cc0813f4d662895ff065b2b719547
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-07 19:49:46 +0100

    additional class

--- Express Js/node-express-course/02-express-tutorial/app.js
+++ Express Js/node-express-course/02-express-tutorial/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    
+const logger = (req, res, next) => {
+    const method = req.method
+    const url = req.url
+    const date = new Date().getFullYear()
+    console.log(method, url, date)
+    next()
+}
+
+app.get('/',logger, (req, res) => {
+    res.send('Home Page')
+})
+
+app.get('/about',logger, (req, res) => {
+     res.send('About Page')  
 })
 
 
